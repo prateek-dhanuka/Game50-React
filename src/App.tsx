b97c5d9b63d0438a187e8a9da015f90be2acdf73
Prateek Dhanuka <prateek.dhanuka03@gmail.com>
2020-11-10 22:39:23 +0530

    Got the loop working

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import "./App.css";
 
-import Circle from "./Circle";
+import Circle from "./elements/Circle";
 import Game from "./Game";
 import React from "react";
 
 function App() {
-  const elements = [
-    <Circle x={100} y={100} maxR={100} color="black" />,
-    <Circle x={200} y={200} maxR={100} color="blue" />,
-    <Circle x={100} y={200} maxR={100} color="red" />,
-    <Circle x={200} y={100} maxR={100} color="green" />,
+  const elements: JSX.Element[] = [
+    <Circle x={100} y={100} maxR={10} color="black" />,
+    <Circle x={200} y={200} maxR={20} color="blue" />,
+    <Circle x={100} y={200} maxR={30} color="red" />,
+    <Circle x={200} y={100} maxR={40} color="green" />,
   ];
 
   return (
     <div className="App">
-      <Game elements={elements} />
+      <Game elements={elements} renderFPS />
     </div>
   );
 }
